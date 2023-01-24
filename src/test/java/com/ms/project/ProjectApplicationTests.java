57e69250cfc409d7439f86cdd6ff0c5d390b955e
misombae <meesomee@naver.com>
2023-01-24 13:47:11 +0900

    project

---
+++ src/test/java/com/ms/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
